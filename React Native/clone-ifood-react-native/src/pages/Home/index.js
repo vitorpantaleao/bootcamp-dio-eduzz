f3962947248b08a99e853cd4b643ca7629a78fe2
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-03-08 00:04:21 -0300

    altera app entry e cria componente home

--- React Native/clone-ifood-react-native/src/pages/Home/index.js
+++ React Native/clone-ifood-react-native/src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import Routes from '../../routes';
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Ifood Clone</Text>
